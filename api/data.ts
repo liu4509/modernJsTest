0856a9e7d6681fbf0d881981a93e49e1e6d05a2d
zhibin <2608606700@qq.com>
2023-01-25 19:31:00 +0800

    一体化 BFF

---
+++ api/data.ts
@@ -0,0 +1,7 @@
+export default async () => {
+  return {
+    title: 'Introduction to Modern.js',
+    content:
+      'Modern.js is a modern web development solution. It offers best practice for different scenarios，such as mobile pages, websites, API services, etc.',
+  };
+};
--- src/pages/introduction.tsx
+++ src/pages/introduction.tsx
@@ -1,5 +1,7 @@
 import { Helmet } from '@modern-js/runtime/head';
 import styled from '@modern-js/runtime/styled';
+import { useState, useEffect } from 'react';
+import getData from '@api/data';
 
 // css in js
 const Title = styled.div`
@@ -7,14 +9,35 @@ const Title = styled.div`
   font-weight: bold;
   text-align: center;
 `;
+const Item = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  text-align: center;
+  color: pink;
+`;
 
-const Introduction = () => (
-  <div>
-    <Helmet>
-      <title>Introduction</title>
-    </Helmet>
-    {/* Utility Class  */}
-    <Title className="text-red-600">Introduction page</Title>
-  </div>
-);
+const Introduction = () => {
+  const [data, setData] = useState<string>();
+
+  useEffect(() => {
+    const load = async () => {
+      // 可以像使用函数一样来调用 API
+      const res = await getData();
+      const _data = JSON.stringify(res);
+      setData(_data);
+    };
+    load();
+  }, []);
+  console.log(data);
+  return (
+    <div>
+      <Helmet>
+        <title>Introduction</title>
+      </Helmet>
+      {/* Utility Class  */}
+      <Title className="text-red-600">Introduction page</Title>
+      {data && <Item>{data}</Item>}
+    </div>
+  );
+};
 export default Introduction;
