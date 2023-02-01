f95a1e76ec7c7a05493d6b342eea92ee39556db0
zhibin <2608606700@qq.com>
2023-02-01 12:09:38 +0800

    配置 Favicon 等通用静态资源

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,14 @@
 import { Helmet } from '@modern-js/runtime/head';
+import styled from '@modern-js/runtime/styled';
+// 配置 Favicon 等通用静态资源
+import demoImg from '@config/public/modernjs-cover.png';
+
+const DemoImg = styled.img`
+  width: 300px;
+  clear: both;
+  display: block;
+  margin: auto;
+`;
 
 const About = () => (
   <div>
@@ -7,6 +17,7 @@ const About = () => (
     </Helmet>
     {/* Utility Class  */}
     <div className="text-center">About page</div>
+    <DemoImg src={demoImg} alt="" />
   </div>
 );
 export default About;
